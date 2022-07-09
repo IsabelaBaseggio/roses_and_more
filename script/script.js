121709f4c4a362759966242e7d32594d7cf323e6
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2022-07-09 05:29:51 -0300

    thumb

--- script/script.js
+++
@@ -1,8 +0,0 @@
-let word = document.getElementById("word");
-
-let circle = word.textContent.split("");
-word.textContent = "";
-
-for(var i = 0; i < circle.length; i++){
-    word.innerHTML += "<span style='transform:rotate(" + (i*8) +"deg)'>"+ circle[i] +"</span>";
-}
